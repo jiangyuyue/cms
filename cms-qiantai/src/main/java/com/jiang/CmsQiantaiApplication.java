c5aa6d2eea56935e5bf3defb65b9e99221a9a193
zhang33 <909807460@qq.com>
2020-06-14 20:05:16 +0800

    Initial commit

---
+++ cms-qiantai/src/main/java/com/jiang/CmsQiantaiApplication.java
@@ -0,0 +1,13 @@
+package com.jiang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CmsQiantaiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CmsQiantaiApplication.class, args);
+    }
+
+}
